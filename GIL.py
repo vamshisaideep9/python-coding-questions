0ee0e702ecc027b729cc43ac0691da05ffc2bfc2
vamshisaideep <vamshisaideep@gmail.com>
2025-03-05 10:58:22 +0530

    gil

---
+++ GIL.py
@@ -0,0 +1,10 @@
+"""
+Global Interpreter Lock- 
+
+It ensures only one thread is executes python bytecode at a time.
+
+Python Uses GIl:
+-> Ensures thread safety in memory management
+-> Improve performance for single-threaded programs
+
+"""
